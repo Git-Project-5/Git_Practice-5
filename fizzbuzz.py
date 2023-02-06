0a9bd075cf8f926665a96858dd153baf6b0a9072
SeoknamKim <rlatjrska1674@gmail.com>
2023-02-06 13:33:35 +0900

    feat: Create Function fizzbuzz()

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,6 @@
-range_num = int(input('Please input range number : '
-print(range_num)
+range_num = int(input('Please input range number : '))
+
+def fizzbuzz(n):
+  for i in range(1, n+1):
+    print(i)
+fizzbuzz(range_num)
