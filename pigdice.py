41444e5d46fbc395f689883c25f5bd87f41c5f4f
sara623 <ysy46331749@gmail.com>
2023-02-06 15:30:25 +0900

    feat: Add Variables and UserTurn, ComputerTurn functions.

---
+++ pigdice.py
@@ -0,0 +1,19 @@
+import random
+
+User_Total = 0
+Computer_Total = 0
+Total_Turn = 10
+Number_Of_Turns = 0
+Computer_Turns = 0
+
+
+def UserTurn():
+    dice = random.randint(1,10)
+    return dice
+
+def ComputerTurn():
+    dice = random.randint(1,10)
+    return dice
+
+
+
